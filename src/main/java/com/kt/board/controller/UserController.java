30cecf8e215ac43e5896fbc0dd316b6c86632215
terry <tryterry77@gmail.com>
2025-11-06 01:40:47 +0900

    feat: Swagger API 연동 및 쿼리 문법 이슈 해결

--- src/main/java/com/kt/board/controller/UserController.java
+++ src/main/java/com/kt/board/controller/UserController.java
@@ -2,6 +2,9 @@ package com.kt.board.controller;
 
 import com.kt.board.domain.dto.request.UserCreateRequest;
 import com.kt.board.service.UserService;
+
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +16,11 @@ public class UserController {
 
     private final UserService userService;
 
+		@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "OK"),
+			@ApiResponse(responseCode = "400", description = "Bad Request"),
+			@ApiResponse(responseCode = "500", description = "Internal Server Error")
+		})
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
     public void create(@RequestBody UserCreateRequest request) {
