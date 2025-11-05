f5935ec72cd9c6f098ec6e1f9258f9aaf94c3d7e
junsang-ee <dkwnstkddk@gmail.com>
2025-11-05 20:59:02 +0900

    feat: spring-doc (swagger) 정의 (In UserController)

--- src/main/java/com/kt/board/controller/UserController.java
+++ src/main/java/com/kt/board/controller/UserController.java
@@ -2,17 +2,27 @@ package com.kt.board.controller;
 
 import com.kt.board.domain.dto.request.UserCreateRequest;
 import com.kt.board.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-@RequiredArgsConstructor
-@RequestMapping("/users")
 @RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+@Tag(name = "유저", description = "유저 API")
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "400", description = "유효성 검사 실패"),
+        @ApiResponse(responseCode = "500", description = "서버 에러 - 백엔드에 바로 문의 바랍니다.")
+})
 public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "유저 생성")
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
     public void create(@RequestBody UserCreateRequest request) {
