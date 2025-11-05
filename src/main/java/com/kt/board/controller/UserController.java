9489494c5e6e4f0689709eb67810a25d657df5cb
l-lyun <kimdohyun032@gmail.com>
2025-11-05 20:26:41 +0900

    docs: UserController Swagger 문서 작성

--- src/main/java/com/kt/board/controller/UserController.java
+++ src/main/java/com/kt/board/controller/UserController.java
@@ -1,21 +1,43 @@
 package com.kt.board.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.kt.board.domain.dto.request.UserCreateRequest;
 import com.kt.board.service.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+@Tag(name = "유저", description = "유저 관련 API")
 @RequiredArgsConstructor
 @RequestMapping("/users")
 @RestController
 public class UserController {
 
-    private final UserService userService;
+	private final UserService userService;
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.OK)
-    public void create(@RequestBody UserCreateRequest request) {
-        userService.create(request);
-    }
+	@Operation(summary = "회원가입 API",
+		responses = {
+			@ApiResponse(
+				responseCode = "200",
+				description = "회원가입 성공"
+			),
+			@ApiResponse(
+				responseCode = "500",
+				description = "백엔드에 문의 바랍니다."
+			)
+		}
+	)
+	@PostMapping
+	@ResponseStatus(HttpStatus.OK)
+	public void create(@RequestBody UserCreateRequest request) {
+		userService.create(request);
+	}
 }
