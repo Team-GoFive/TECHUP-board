449efc89332284edb2b5f3e45dad39c5aed3ada7
최민서 <114375668+oneokiwa@users.noreply.github.com>
2025-11-06 01:58:23 +0900

    feature: swagger 의존성 추가 및 계정 생성 API 추가

--- src/main/java/com/kt/board/controller/UserController.java
+++ src/main/java/com/kt/board/controller/UserController.java
@@ -5,13 +5,20 @@ import com.kt.board.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+// 클래스 레벨에 API 그룹 설정
+@Tag(name = "User API", description = "사용자 계정 생성 및 관리 API")
 @RequiredArgsConstructor
 @RequestMapping("/users")
 @RestController
 public class UserController {
 
     private final UserService userService;
+
+		@Operation(summary = "새로운 사용자 계정 생성",
+			description = "새로운 사용자를 등록합니다.")
 
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
