af5cdb284894e74ff4c4ed4f04dd69015a629cee
최민서 <114375668+oneokiwa@users.noreply.github.com>
2025-11-07 00:08:10 +0900

    feat: Board update 구현

--- src/main/java/com/kt/board/controller/BoardController.java
+++ src/main/java/com/kt/board/controller/BoardController.java
@@ -3,12 +3,14 @@ package com.kt.board.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kt.board.domain.dto.request.BoardCreateRequest;
+import com.kt.board.domain.dto.request.BoardUpdateRequest;
 import com.kt.board.domain.dto.request.PostCreateRequest;
 import com.kt.board.service.BoardService;
 import com.kt.board.service.PostService;
@@ -32,5 +34,11 @@ public class BoardController {
 	@ResponseStatus(HttpStatus.OK)
 	public void create(@PathVariable Long boardId, @RequestBody PostCreateRequest request) {
 		postService.create(boardId, request);
+	}
+
+	@PutMapping("/{boardId}")
+	@ResponseStatus(HttpStatus.OK)
+	public void update(@PathVariable Long boardId, @RequestBody BoardUpdateRequest request) {
+		boardService.update(boardId, request);
 	}
 }
