c6016d648e0c2628df75137336986f9387b36cae
junsang-ee <dkwnstkddk@gmail.com>
2025-11-09 22:43:12 +0900

    refactor: 게시판 update Status 필드 제거

--- src/main/java/com/kt/board/domain/dto/request/BoardUpdateRequest.java
+++ src/main/java/com/kt/board/domain/dto/request/BoardUpdateRequest.java
@@ -1,15 +1,10 @@
 package com.kt.board.domain.dto.request;
 
-import com.kt.board.constants.BoardStatus;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record BoardUpdateRequest(
 
         @NotBlank
-	    String name,
-
-        @NotNull
-        BoardStatus status
+	    String name
 ) {
 }
