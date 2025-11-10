e58e1354d13446f044b6336b770df6237e87f9cc
TopOfHead <164000692+FA-50@users.noreply.github.com>
2025-11-10 09:37:32 +0900

    feat: doc ( swagger ) 기능 추가 (#6)

--- src/main/java/com/kt/board/controller/UserController.java
+++ src/main/java/com/kt/board/controller/UserController.java
@@ -2,10 +2,17 @@ package com.kt.board.controller;
 
 import com.kt.board.domain.dto.request.UserCreateRequest;
 import com.kt.board.service.UserService;
+
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@ApiResponses(value = {
+	@ApiResponse(responseCode="400", description = "유효성 검사 실패"),
+	@ApiResponse(responseCode = "500", description = "서버 에러")
+})
 @RequiredArgsConstructor
 @RequestMapping("/users")
 @RestController
